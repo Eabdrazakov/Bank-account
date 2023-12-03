a730d3c6e4b2e6795de31b2598a936a469d36806
Padma Patil <padma@email.com>
2023-12-02 21:28:07 -0800

    Add UI logic

--- js/scripts.js
+++ js/scripts.js
@@ -28,11 +28,11 @@ UserAccount.prototype.transaction = function (amount) {
 //UI logic
 
 const userDataBase = new UserDataBase();
-const user1 = new UserAccount("Ermek", 1000);
-const user2 = new UserAccount("Adam", 500);
+// const user1 = new UserAccount("Ermek", 1000);
+// const user2 = new UserAccount("Adam", 500);
 
-userDataBase.addUser(user1);
-userDataBase.addUser(user2);
+// userDataBase.addUser(user1);
+// userDataBase.addUser(user2);
 
 
 function transaction(userId, type, amount) {
@@ -56,12 +56,40 @@ function transaction(userId, type, amount) {
 function accountFormHandler(e) {
     e.preventDefault();
 
+    const name = document.getElementById("name").value;
+    const inDeposit = parseFloat(document.getElementById("initial").value);
+    const selected = document.getElementById("transactions").value;
+    const amount = parseFloat(document.getElementById("amount").value);
+
+    const result = document.getElementById("result");
+
+    //Displaying the results
+    result.classList.remove("hidden");
+    const userId = userDataBase.currentId + 1;
+    const newUser = new UserAccount(name, inDeposit);
+    userDataBase.addUser(newUser);
+
+    transaction(userId, selected, amount);
+
+    document.getElementById("number").value = userDataBase.users[userId].balance;
+}
+
+function reset(e) {
+    e.preventDefault();
+    const result = document.getElementById("result");
+    document.getElementById("name").value = "";
+    document.getElementById("initial").value = "";
+    document.getElementById("transactions").value = "deposit";
+    document.getElementById("amount").value = "";
+
+    result.classList.add("hidden");
 }
 
 
 
 window.addEventListener("load", function () {
-    document.querySelector("form").addEventListener("submit");
+    document.querySelector("form").addEventListener("submit", accountFormHandler);
+    document.querySelector("form").addEventListener("reset", reset);
 });
 // console.log(userDataBase);
 
